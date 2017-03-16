3450ccf1723665563cf4617d3d049f64516cae47
Christian Schlichtherle <christian@schlichtherle.de>
2017-03-16 06:27:25 +0100

    Adds POC.

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,13 +1,20 @@
 package controllers
 
 import javax.inject._
-import play.api._
+
+import models.ConsoleGame
 import play.api.mvc._
+import views.html._
 
 @Singleton
 class HomeController extends Controller {
 
+  private val game = ConsoleGame(20, 200)
+  private val toString = game.render
+  import game._
+
   def index = Action { implicit request =>
-    Ok(views.html.index())
+    val text = (game iterator Board() take 10 map toString).mkString
+    Ok(main("Conway's Game of Life")(pre(text)))
   }
 }
